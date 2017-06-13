f3fdd982725108942b7bc6610cfc55d3b2af1baf
br0r <viktor@silfverstrom.com>
2017-06-13 11:33:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# node JSON bulk stream
+
+## usage
+```bash
+#data.json is newline delimited objects   
+<data.json nbulk 10
+```
---
+++ bin/nbulk.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var split = require('split');
+var Transform = require('stream').Transform;
+var os = require('os');
+
+var n = parseInt(process.argv[2]);
+if (!n || n <= 0) exit('Invalid number');
+
+var b = [];
+var t = new Transform({
+  writableObjectMode: true,
+  transform: function (chunk, enc, next) {
+    b.push(chunk);
+    if (b.length >= n) {
+      this.push(JSON.stringify(b.splice(0, n)) + os.EOL);
+    }
+
+    next();
+  },
+
+  flush: function (cb) {
+    this.push(JSON.stringify(b) + os.EOL);
+    cb();
+  },
+
+});
+
+process.stdin
+  .pipe(split(JSON.parse, null, { trailing: false }))
+  .on('error', exit)
+  .pipe(t)
+  .on('error', exit)
+  .pipe(process.stdout);
+
+function exit(msg) {
+  console.error(msg);
+  process.exit(msg ? 1 : 0);
+}
